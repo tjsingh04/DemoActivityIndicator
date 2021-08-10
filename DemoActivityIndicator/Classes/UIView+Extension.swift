9382013ea91294fac4757c257ba73abadf440678
tjsingh04 <tejinder@zimblecode.com>
2021-08-10 13:58:47 +0530

    font size change

--- DemoActivityIndicator/Classes/UIView+Extension.swift
+++ DemoActivityIndicator/Classes/UIView+Extension.swift
@@ -134,7 +134,7 @@ public extension UIView {
             
             let label:UILabel = UILabel(frame: CGRect.zero)
             label.translatesAutoresizingMaskIntoConstraints = false
-            label.font = UIFont.systemFont(ofSize: 15.0)
+            label.font = UIFont.systemFont(ofSize: 30)
             label.text = withMessage
             label.numberOfLines = 2
             label.textAlignment = .center
